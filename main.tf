4330489f8a6caab5cf0fb602bfbabc3f1f586db6
jwdm99 <134312068+jwdm99@users.noreply.github.com>
2023-10-12 17:30:51 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -86,3 +86,47 @@ resource "azurerm_subnet_route_table_association" "sn-rt-avd-prod-scus-01" {
   subnet_id      = azurerm_subnet.Subnet-AVD-Prod-SC-001.id
   route_table_id = azurerm_route_table.rt-avd-prod-scus-01.id
 }
+
+#Create Host Pool "DH-AVD-PROD" NO SCALING PLAN -YET-
+resource "azurerm_virtual_desktop_host_pool" "DH-AVD-PROD" {
+  location            = azurerm_resource_group.rg-avd-prod-scus.location
+  resource_group_name = azurerm_resource_group.rg-avd-prod-scus.name
+
+  name                     = "DH-AVD-PROD"
+  friendly_name            = "DH-AVD-PROD"
+  validate_environment     = true
+  start_vm_on_connect      = true
+  custom_rdp_properties    = "audiocapturemode:i:1;audiomode:i:0;"
+  description              = "Acceptance Test: A pooled host pool - pooleddepthfirst"
+  type                     = "Pooled"
+  maximum_sessions_allowed = 50
+  load_balancer_type       = "DepthFirst"
+}
+
+#Create Desktop Application Group "DH-AVD-PROD-DAG"
+resource "azurerm_virtual_desktop_application_group" "DH-AVD-PROD-DAG" {
+  name                = "DH-AVD-PROD-DAG"
+  location            = azurerm_resource_group.rg-avd-prod-scus.location
+  resource_group_name = azurerm_resource_group.rg-avd-prod-scus.name
+
+  type          = "Desktop"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.DH-AVD-PROD.id
+  friendly_name = "Default Desktop"
+  description   = "Desktop Application Group created through the Hostpool Wizard"
+}
+
+#Create Workspace "DH-AVD-PROD"
+resource "azurerm_virtual_desktop_workspace" "DH-AVD-PROD" {
+  name                = "DH-AVD-PROD"
+  location            = azurerm_resource_group.rg-avd-prod-scus.location
+  resource_group_name = azurerm_resource_group.rg-avd-prod-scus.name
+
+  friendly_name = "DH-AVD-PROD"
+  description   = "Production Workspace"
+}
+
+#Associate Workspace to DAG
+resource "azurerm_virtual_desktop_workspace_application_group_association" "AVD-PROD-DAG" {
+  workspace_id         = azurerm_virtual_desktop_workspace.DH-AVD-PROD.id
+  application_group_id = azurerm_virtual_desktop_application_group.DH-AVD-PROD-DAG.id
+}
